efaec6c0139e558649b46007e3c5088fa1aea79f
zhangyong <zhangyong7120180@163.com>
2014-05-19 13:57:16 +0800

    添加dequee test

---
+++ src/test/java/DelayQueueTest.java
@@ -0,0 +1,15 @@
+/**
+ * 测试DelayQueue 是参考http://www.2cto.com/kf/201203/123939.html这个例子 也可以看
+ * http://www.cnblogs.com/jobs/archive/2007/04/27/730255.html 温少写的例子
+ * User: zhangyong
+ * Date: 14-1-3
+ * Time: 上午10:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class DelayQueueTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
